4dcc9b4cb2d0c1a219e5584950a32a8f77cd28d8
David Hu <davidhu314@gmail.com>
2017-05-06 21:05:47 -0700

    update ts config, fixed type check in root

--- src/swap.tsx
+++ src/swap.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Store } from 'redux';
 import configureStore from './core/store';
 import Root from './core/root';
 
@@ -9,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let preloadedState = {
     user: JSON.parse(localStorage.getItem('google-user'))
   };
-  const store = configureStore(preloadedState);
+  const store: Store<any> = configureStore(preloadedState);
 
   ReactDOM.render(<Root store={store}/>, root);
   // window.store = store;
