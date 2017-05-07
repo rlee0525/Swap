f62f43668bbaf6067585c38fbfd07f0271003e67
randyjap <randyjap@gmail.com>
2017-05-06 20:17:14 -0700

    setup rspec

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,14 @@ module Swapnow
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => false,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => false
+    end
   end
 end
