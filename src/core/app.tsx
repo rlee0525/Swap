24b4bd643d0dd57480fda22ae90bb04f230fb235
Raymond Lee <raylee0525@gmail.com>
2017-05-07 15:40:35 -0700

    Implement baseurl to allow absolute path to include ./src

--- src/core/app.tsx
+++ src/core/app.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
-import HomeContainer from 'modules/home'
-const App = ({ children }) => (
+import * as React from 'react';
+import HomeContainer from 'modules/home';
+
+
+interface IRootProps {
+  children?: any;
+}
+
+const App: React.SFC<IRootProps> = ({ children }) => (
   <div className="home">
     <div className="home-overlay">
       <HomeContainer />
--- src/modules/home/component.tsx
+++ src/modules/home/component.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 
-class Home extends React.Component {
-  constructor(props) {
+class Home extends React.Component<any, any> {
+  constructor(props: any) {
     super(props);
 
   }
 
   componentDidMount() {
-
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=1843174039341965";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
   }
 
   render() {
+    console.log(this.props);
     return (
       <div>
         Hi
-        <div className="fb-login-button" data-max-rows="1" data-size="large"
-             data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false">
-        </div>
+        <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
       </div>
     )
   }
