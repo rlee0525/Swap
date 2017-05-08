445301404ca98300e17c007ae945243e1a286855
Raymond Lee <raylee0525@gmail.com>
2017-05-07 23:35:29 -0700

    move script files

--- src/modules/home/component.tsx
+++ src/modules/home/component.tsx
@@ -6,69 +6,7 @@ class Home extends React.Component<any, any> {
   }
 
   public componentDidMount() {
-    window.fbAsyncInit = function() {
-      FB.init({
-        appId: '1843174039341965',
-        cookie: true,
-        xfbml: true,
-        version: 'v2.1'
-      });
 
-      FB.getLoginStatus(function(response) {
-        console.log(response);
-        this.statusChangeCallback(response);
-      }.bind(this));
-    }.bind(this);
-
-    ((d, s, id) => {
-      let js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.setAttribute('src', "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=1843174039341965");
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, 'script', 'facebook-jssdk');
-  }
-
-  public testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-    console.log('Successful login for: ' + response.name);
-    document.getElementById('status').innerHTML =
-      'Thanks for logging in, ' + response.name + '!';
-    });
-  }
-
-  public statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      this.testAPI();
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-      'into Facebook.';
-    }
-  }
-
-  public checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      console.log(response)
-      this.statusChangeCallback(response);
-    }.bind(this));
-  }
-
-  public handleClick() {
-    FB.login(this.checkLoginState());
   }
 
   public render() {
--- src/modules/signup/component.tsx
+++ src/modules/signup/component.tsx
@@ -7,7 +7,71 @@ class SignUp extends React.Component<any, any> {
 
   public componentDidMount() {
     this.threeStepForm();
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId: '1843174039341965',
+        cookie: true,
+        xfbml: true,
+        version: 'v2.1'
+      });
+
+      FB.getLoginStatus(function(response) {
+        console.log(response);
+        this.statusChangeCallback(response);
+      }.bind(this));
+    }.bind(this);
+
+    ((d, s, id) => {
+      let js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.setAttribute('src', "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=1843174039341965");
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
   }
+
+  public testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+    console.log('Successful login for: ' + response.name);
+    document.getElementById('status').innerHTML =
+      'Thanks for logging in, ' + response.name + '!';
+    });
+  }
+
+  public statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      this.testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+      'into Facebook.';
+    }
+  }
+
+  public checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      console.log(response)
+      this.statusChangeCallback(response);
+    }.bind(this));
+  }
+
+  public handleClick() {
+    FB.login(this.checkLoginState());
+  }
+
 
   public threeStepForm() {
     var current_fs, next_fs, previous_fs;
@@ -81,7 +145,7 @@ class SignUp extends React.Component<any, any> {
     console.log(this.props);
 
     return (
-      <div>
+      <div className="signup-form-container">
         <form id="msform">
           <ul id="progressbar">
             <li className="active">Account Setup</li>
@@ -91,28 +155,27 @@ class SignUp extends React.Component<any, any> {
 
           <fieldset>
             <h2 className="fs-title">Create your account</h2>
-            <h3 className="fs-subtitle">This is step 1</h3>
-            <input type="text" name="email" placeholder="Email" />
-            <input type="password" name="pass" placeholder="Password" />
-            <input type="password" name="cpass" placeholder="Confirm Password" />
+            <div id="status"></div>
+            <div
+              className="fb-login-button"
+              data-max-rows="1"
+              data-size="large"
+              data-button-type="continue_with"
+              data-show-faces="true"
+               data-auto-logout-link="true"
+               data-use-continue-as="true"
+             />
             <input type="button" name="next" className="next action-button" value="Next" />
           </fieldset>
           <fieldset>
-            <h2 className="fs-title">Social Profiles</h2>
-            <h3 className="fs-subtitle">Your presence on the social network</h3>
-            <input type="text" name="twitter" placeholder="Twitter" />
-            <input type="text" name="facebook" placeholder="Facebook" />
-            <input type="text" name="gplus" placeholder="Google Plus" />
+            <h2 className="fs-title">Link to your university email</h2>
+            <input type="text" name="uni_email" placeholder="University Email" />
             <input type="button" name="previous" className="previous action-button" value="Previous" />
             <input type="button" name="next" className="next action-button" value="Next" />
           </fieldset>
           <fieldset>
-            <h2 className="fs-title">Personal Details</h2>
-            <h3 className="fs-subtitle">We will never sell it</h3>
-            <input type="text" name="fname" placeholder="First Name" />
-            <input type="text" name="lname" placeholder="Last Name" />
-            <input type="text" name="phone" placeholder="Phone" />
-            <textarea name="address" placeholder="Address"></textarea>
+            <h2 className="fs-title">Email Verification</h2>
+            <h3 className="fs-subtitle">Thank you for verifying your email!</h3>
             <input type="button" name="previous" className="previous action-button" value="Previous" />
             <input type="submit" name="submit" className="submit action-button" value="Submit" />
           </fieldset>
