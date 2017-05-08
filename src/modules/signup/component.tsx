8f6565089677d02641f8b0cd1322ca1206ba08b4
Raymond Lee <raylee0525@gmail.com>
2017-05-07 22:58:31 -0700

    render multistep form

--- src/modules/signup/component.tsx
+++ src/modules/signup/component.tsx
@@ -5,6 +5,10 @@ class SignUp extends React.Component<any, any> {
     super(props);
   }
 
+  public componentDidMount() {
+    this.threeStepForm();
+  }
+
   public threeStepForm() {
     var current_fs, next_fs, previous_fs;
     var left, opacity, scale;
@@ -78,41 +82,39 @@ class SignUp extends React.Component<any, any> {
 
     return (
       <div>
-        <!-- multistep form -->
         <form id="msform">
-          <!-- progressbar -->
           <ul id="progressbar">
-            <li class="active">Account Setup</li>
+            <li className="active">Account Setup</li>
             <li>Social Profiles</li>
             <li>Personal Details</li>
           </ul>
-          <!-- fieldsets -->
+
           <fieldset>
-            <h2 class="fs-title">Create your account</h2>
-            <h3 class="fs-subtitle">This is step 1</h3>
+            <h2 className="fs-title">Create your account</h2>
+            <h3 className="fs-subtitle">This is step 1</h3>
             <input type="text" name="email" placeholder="Email" />
             <input type="password" name="pass" placeholder="Password" />
             <input type="password" name="cpass" placeholder="Confirm Password" />
-            <input type="button" name="next" class="next action-button" value="Next" />
+            <input type="button" name="next" className="next action-button" value="Next" />
           </fieldset>
           <fieldset>
-            <h2 class="fs-title">Social Profiles</h2>
-            <h3 class="fs-subtitle">Your presence on the social network</h3>
+            <h2 className="fs-title">Social Profiles</h2>
+            <h3 className="fs-subtitle">Your presence on the social network</h3>
             <input type="text" name="twitter" placeholder="Twitter" />
             <input type="text" name="facebook" placeholder="Facebook" />
             <input type="text" name="gplus" placeholder="Google Plus" />
-            <input type="button" name="previous" class="previous action-button" value="Previous" />
-            <input type="button" name="next" class="next action-button" value="Next" />
+            <input type="button" name="previous" className="previous action-button" value="Previous" />
+            <input type="button" name="next" className="next action-button" value="Next" />
           </fieldset>
           <fieldset>
-            <h2 class="fs-title">Personal Details</h2>
-            <h3 class="fs-subtitle">We will never sell it</h3>
+            <h2 className="fs-title">Personal Details</h2>
+            <h3 className="fs-subtitle">We will never sell it</h3>
             <input type="text" name="fname" placeholder="First Name" />
             <input type="text" name="lname" placeholder="Last Name" />
             <input type="text" name="phone" placeholder="Phone" />
             <textarea name="address" placeholder="Address"></textarea>
-            <input type="button" name="previous" class="previous action-button" value="Previous" />
-            <input type="submit" name="submit" class="submit action-button" value="Submit" />
+            <input type="button" name="previous" className="previous action-button" value="Previous" />
+            <input type="submit" name="submit" className="submit action-button" value="Submit" />
           </fieldset>
         </form>
       </div>
