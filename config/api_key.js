45d4df22e4ee8e155e4bec803f8ce70e9e921aea
Raymond Lee <raylee0525@gmail.com>
2017-05-07 18:53:08 -0700

    delete api_key

--- config/api_key.js
+++ config/api_key.js
@@ -1,5 +1,5 @@
-module.exports = {
-  clientId: '781815846323-200t7iljesmblf40hs46ravmo2eqeb7l.apps.googleusercontent.com',
-  clientSecret: '4AlG8f4oPhyEzLjHL7fvQ77a',
-  publicDataKey: 'AIzaSyD3CqypMYSV20_E2ub_imNu3wKiT76b8C8'
-};
+// module.exports = {
+//   clientId: '781815846323-200t7iljesmblf40hs46ravmo2eqeb7l.apps.googleusercontent.com',
+//   clientSecret: '4AlG8f4oPhyEzLjHL7fvQ77a',
+//   publicDataKey: 'AIzaSyD3CqypMYSV20_E2ub_imNu3wKiT76b8C8'
+// };
