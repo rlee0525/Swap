6d9ccb78543a7c0efa1bec6454e364a6319dd777
Raymond Lee <raylee0525@gmail.com>
2017-05-10 09:54:17 -0700

    setting up routes and links to all the components

--- src/core/footer.tsx
+++ src/core/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => (
   <div className="block block-bordered-lg" id="footer-block">
     <div className="container">
       <div className="flex-row">
-        <h6 className="text-uppercase">About</h6>
+        <a><h6 className="text-uppercase">About</h6></a>
         <h6 className="text-uppercase">Careers</h6>
         <h6 className="text-uppercase">Help</h6>
         <h6 className="text-uppercase">Contact</h6>
