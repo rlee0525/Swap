23f23e9d3fd8fb887e01de0739016a54bf1a3a37
David Hu <davidhu314@gmail.com>
2017-05-10 20:39:25 -0700

    add algorithm to calculate string match

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -2,14 +2,48 @@ class Api::SearchController < ApplicationController
   def index
     # category = params[:query].capitalize
     # @posts = Post.joins(:category).where('categories.name = ?', category)
+    query = params[:query]
 
-
-    query = params[:query].split(' ').map { |word| "%#{word}%" }
+    sql_query = query.split(' ').map { |word| "%#{word}%" }
 
     sql = 'title ILIKE ANY( array[?] ) OR categories.name ILIKE ANY ( array[?] )'
 
-    @posts = Post.joins(:category).where(sql, query, query)
+    @posts = Post.joins(:category).where(sql, sql_query, sql_query)
+
+    
 
     render 'api/search/index'
+  end
+
+  private 
+
+  def match(str1, str2)
+    matrix = Array.new(str1.length + 1) { Array.new(str2.length + 1, nil) }
+    matrix[0] = (0..str1.length + 1).to_a
+    
+    matrix.each_with_index do |row, r_idx|
+      next if r_idx.zero?
+      
+      row.each_with_index do |el, c_idx|
+        if c_idx.zero?
+          matrix[r_idx][c_idx] = matrix[r_idx - 1][c_idx] + 1
+          next
+        end
+        up = matrix[r_idx - 1][c_idx]
+        left = matrix[r_idx][c_idx - 1]
+        dia = matrix[r_idx - 1][c_idx - 1]
+        
+        min = [up, left, dia].min
+        
+        if str1[r_idx - 1].downcase == str2[c_idx - 1].downcase
+          matrix[r_idx][c_idx] = min
+        else
+          matrix[r_idx][c_idx] = min + 1
+        end
+    
+      end
+    end
+    
+    str1.length - matrix.last.last
   end
 end
