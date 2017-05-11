a913df2adf0ea4ca9aef4ef390bb957b86c6e708
David Hu <davidhu314@gmail.com>
2017-05-10 20:36:18 -0700

    add function to query database on title and categories

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,7 +1,15 @@
 class Api::SearchController < ApplicationController
   def index
-    category = params[:query].capitalize
-    @posts = Post.joins(:category).where('categories.name = ?', category)
+    # category = params[:query].capitalize
+    # @posts = Post.joins(:category).where('categories.name = ?', category)
+
+
+    query = params[:query].split(' ').map { |word| "%#{word}%" }
+
+    sql = 'title ILIKE ANY( array[?] ) OR categories.name ILIKE ANY ( array[?] )'
+
+    @posts = Post.joins(:category).where(sql, query, query)
+
     render 'api/search/index'
   end
 end
