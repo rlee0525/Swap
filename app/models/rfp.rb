95d2d332c3bb9c4ef803f18f7b0172b087c2c3c7
randyjap <randyjap@gmail.com>
2017-05-15 07:34:31 -0700

    annotate rfp

--- app/models/rfp.rb
+++ app/models/rfp.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: rfps
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer          not null
+#  description :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Rfp < ApplicationRecord
   validates :user, :description, presence: true
   belongs_to :user
--- spec/models/rfp_spec.rb
+++ spec/models/rfp_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: rfps
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer          not null
+#  description :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Rfp, type: :model do
