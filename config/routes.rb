c172d0811cbc52c29283e3b62c6e0acb4559b19e
randyjap <randyjap@gmail.com>
2017-05-15 13:29:15 -0700

    add rfps routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
       resources :categories, only: [:index, :show]
       resources :search, only: [:index]
       resources :bookmarks, only: [:index, :create, :destroy]
+      resources :rfps, only: [:index, :create, :destroy]
     end
   end
 
