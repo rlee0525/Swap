b6ec5d663bf41164f6d777ddf36ccd3f31d4ebf9
randyjap <randyjap@gmail.com>
2017-05-15 18:52:17 -0700

    add ordering in search

--- src/modules/search/subcomponents/search_list_view.tsx
+++ src/modules/search/subcomponents/search_list_view.tsx
@@ -22,6 +22,16 @@ interface State {
 class SearchListView extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
+    this.sortBy = this.sortBy.bind(this);
+    let results = this.props.searchResult;
+    this.state = {
+      title: -1,
+      description: -1,
+      price: -1,
+      created_at: -1,
+      condition: -1,
+      results
+    };
 
     this.checkVerified = this.checkVerified.bind(this);
   }
@@ -60,20 +70,34 @@ class SearchListView extends React.Component<Props, State> {
     )
   }
 
+  public sortBy(key) {
+    let polarity = this.state[key];
+    let newArray = this.state.results.sort(function(a, b) {
+      if (a[key] < b[key]) return (-1 * polarity);
+      if (a[key] > b[key]) return (1 * polarity);
+      return 0;
+    })
+    let newPolarity = (polarity === -1 ? 1 : -1);
+    this.setState({
+      posts: newArray,
+      [key]: newPolarity
+    });
+  }
+
   render() {
     return (
       <table className="table table-hover">
         <thead>
           <tr>
-            <th>Title</th>
-            <th className="hidden-xs">Description</th>
-            <th>Price</th>
-            <th className="hidden-xs">Posting Date</th>
-            <th className="hidden-xs">Condition</th>
+            <th>Title<a onClick={() => this.sortBy("title")} className="btn btn-xs" ><span className="caret" /></a></th>
+            <th className="hidden-xs">Description<a onClick={() => this.sortBy("description")} className="btn btn-xs" ><span className="caret" /></a></th>
+            <th>Price<a onClick={() => this.sortBy("price")} className="btn btn-xs" ><span className="caret" /></a></th>
+            <th className="hidden-xs">Posting Date<a onClick={() => this.sortBy("created_at")} className="btn btn-xs" ><span className="caret" /></a></th>
+            <th className="hidden-xs">Condition<a onClick={() => this.sortBy("condition")} className="btn btn-xs" ><span className="caret" /></a></th>
           </tr>
         </thead>
         <tbody>
-          { this.props.searchResult ? this.props.searchResult.map((post,idx) => this.renderListItem(post, idx)) : null }
+          { this.state.results ? this.state.results.map((post,idx) => this.renderListItem(post, idx)) : null }
         </tbody>
       </table>
     )
