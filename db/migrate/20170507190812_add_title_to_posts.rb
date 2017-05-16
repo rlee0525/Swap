cc5b8731f7f31209821d58a65c3c35edcef29e1a
randyjap <randyjap@gmail.com>
2017-05-16 01:07:14 -0700

    clean up migrations merge with master

--- db/migrate/20170507190812_add_title_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddTitleToPosts < ActiveRecord::Migration[5.0]
-  def change
-    add_column :posts, :title, :string, null: false
-  end
-end
--- db/migrate/20170509171055_change_users_fb_id_integer_to_string.rb
+++
@@ -1,6 +0,0 @@
-class ChangeUsersFbIdIntegerToString < ActiveRecord::Migration[5.0]
-  def change
-    remove_colum :users, :fb_id
-    add_column :users, :fb_id, :string, null: false
-  end
-end
