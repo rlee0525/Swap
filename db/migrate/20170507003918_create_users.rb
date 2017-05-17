a733af18872cb5d0973612d2df0120410e58cdd4
randyjap <randyjap@gmail.com>
2017-05-16 18:33:01 -0700

    change fb_id to string

--- db/migrate/20170507003918_create_users.rb
+++ db/migrate/20170507003918_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.integer :fb_id, null: false
+      t.string :fb_id, null: false
       t.boolean :edu_email_confirmed, default: false
       t.string :edu_email_confirm_token
       t.string :fb_email
