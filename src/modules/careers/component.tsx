c21734f42c574ac6c0befd9f88d2295bc3263ccc
randyjap <randyjap@gmail.com>
2017-05-18 16:07:08 -0700

    add about, careers, faq, contact page

--- src/modules/careers/component.tsx
+++ src/modules/careers/component.tsx
@@ -7,8 +7,16 @@ class Careers extends React.Component<any, any> {
 
   public render() {
     return (
-      <div>
-        We currently do not have openings. Please check again later!
+      <div className="block app-price-plans">
+        <div className="container text-center">
+          <div className="row m-b-lg">
+            <div className="col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+              <h6 className="text-muted text-uppercase">We'd love to hear from you</h6>
+              <h6 className="text-muted text-uppercase">Drop us a line at</h6>
+              <h6 className="text-muted text-uppercase">swapnowio[at]gmail.com</h6>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
