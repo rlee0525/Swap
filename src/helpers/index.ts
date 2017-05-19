cb2acdb735bb488fb04d29a4e5e64dc0dd297a8a
David Hu <davidhu314@gmail.com>
2017-05-18 20:15:29 -0700

    update typings for helpers

---
+++ src/helpers/index.ts
@@ -0,0 +1,31 @@
+export const timeFromNow = (date: string) : string => {
+  let durationsInSeconds: object = {
+    year: 31536000,
+    month: 2592000,
+    day: 86400,
+    hour: 3600,
+    minute: 60,
+    second: 1
+  };
+
+  let ms:number = Math.floor((Number(new Date()) - Number(new Date(date))) / 1000);
+  let types: string[] = ['year', 'month', 'day', 'hour', 'minute', 'second'];
+
+  for (let idx = 0; idx < types.length; idx++) {
+    let num: number = Math.floor(ms / durationsInSeconds[types[idx]]);
+    if (num >= 1) {
+      let type: string = types[idx];
+      if (num > 1) type += 's';
+      return `${num} ${type} ago`;
+    }
+  }
+};
+
+// add ... to strings that are too long
+export const shortenString = (string: string, maxLength: number) : string => {
+  if (string.length > maxLength) {
+    let idx: number = maxLength - 3;
+    string = `${string.slice(0, idx)}...`;
+  }
+  return string;
+};
