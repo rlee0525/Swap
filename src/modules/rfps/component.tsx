9a513a78ad97589b7ab863eac949d77eba80c1ca
David Hu <davidhu314@gmail.com>
2017-05-18 22:50:35 -0700

    update formatting for component

--- src/modules/rfps/component.tsx
+++ src/modules/rfps/component.tsx
@@ -17,12 +17,12 @@ interface Props {
   user: object;
 }
 
-class Rfps extends React.Component<Props, State> {
-  constructor(props: any) {
+class Rfps extends React.Component <Props, State> {
+  constructor(props : any) {
     super(props);
-    this.state = { 
-      rfps: [], 
-      description: -1 
+    this.state = {
+      rfps: [],
+      description: -1
     };
 
     this.fetchRfps = this.fetchRfps.bind(this);
@@ -31,16 +31,16 @@ class Rfps extends React.Component<Props, State> {
 
   public fetchRfps() {
     $.ajax({
-      method: "GET",
+      method: "GET", 
       url: "api/rfps"
     }).then(
-      rfps => this.setState({ rfps })
+      rfps => this.setState({rfps})
     );
   }
 
   public deleteRfp(id) {
     $.ajax({
-      type: "DELETE",
+      type: "DELETE", 
       url: `api/rfps/${id}`
     }).then(
       data => this.fetchRfps()
@@ -52,11 +52,15 @@ class Rfps extends React.Component<Props, State> {
   }
 
   public renderListItems() {
-    return this.state.rfps.map((rfp: RFP) => (
+    return this.state.rfps.map((rfp : RFP) => (
       <tr key={`post${rfp.id}`}>
         <td>{shortenString(rfp.description, 30)}</td>
         <td>
-          <button type="button" className="btn btn-xs btn-danger" onClick={() => this.deleteRfp(rfp.id)}>
+          <button
+            type="button"
+            className="btn btn-xs btn-danger"
+            onClick={() => this.deleteRfp(rfp.id)}
+          >
             Delete
           </button>
         </td>
@@ -65,45 +69,43 @@ class Rfps extends React.Component<Props, State> {
   }
 
   public sortBy(key) {
-    let polarity: number = this.state[key];
-    let newArray: object[] = this.state.rfps.sort(function(a, b) {
+    let polarity : number = this.state[key];
+    let newArray : object[] = this.state.rfps.sort(function (a, b) {
       if (a[key] < b[key]) return (-1 * polarity);
       if (a[key] > b[key]) return (1 * polarity);
       return 0;
     });
-    
-    let newPolarity: number = (polarity === -1 ? 1 : -1);
-    this.setState({
-      rfps: newArray,
-      [key]: newPolarity
-    });
+
+    let newPolarity : number = -1 * polarity;
+    this.setState({rfps: newArray, [key]: newPolarity});
   }
 
   public render() {
     return (
       <div>
         <div className="container">
-          <NavTabs />
-          <div>
-            <div className="panel panel-default">
-              <div className="panel-body">
-                <table className="table table-hover">
-                  <thead>
-                    <tr>
-                      <th>
-                        Description
-                        <a onClick={() => this.sortBy("description")} className="btn btn-xs" >
-                          <span className="caret" />
-                        </a>
-                      </th>
-                      <th className="col-xs-1">Delete</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {this.renderListItems()}
-                  </tbody>
-                </table>
-              </div>
+          <NavTabs/>
+          <div className="panel panel-default">
+            <div className="rfp-description" style={{ width: "100%", padding: "15px" }} >
+              Create custom alerts to get emails whenever a posting related to the created keywords.
+            </div>
+            <div className="panel-body">
+              <table className="table table-hover">
+                <thead>
+                  <tr>
+                    <th>
+                      <a onClick={() => this.sortBy("description")} className="btn btn-xs">
+                        Keywords
+                        <span className="caret"/>
+                      </a>
+                    </th>
+                    <th className="col-xs-1">Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.renderListItems()}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
