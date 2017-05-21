70cdbc22bbbc5c12881d15cee1f5c35cb2f58274
David Hu <davidhu314@gmail.com>
2017-05-20 17:43:50 -0700

    update footer and creates infos module

---
+++ src/modules/infos/component.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Careers extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+  }
+
+  public render() {
+
+    let page = this.props.location.search;
+
+    console.log(page);
+    
+    
+    return (
+      <div className="block app-price-plans">
+        <div className="container text-center">
+          <div className="row m-b-lg">
+            <div className="col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+              <h6 className="text-muted text-uppercase">We'd love to hear from you</h6>
+              <h6 className="text-muted text-uppercase">Drop us a line at</h6>
+              <h6 className="text-muted text-uppercase">swapnowio[at]gmail.com</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Careers;
