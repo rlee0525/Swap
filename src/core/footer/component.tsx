efdf1ee3c0753af62ae6b6c91fce6b901f5ab004
David Hu <davidhu314@gmail.com>
2017-05-20 17:33:58 -0700

    update footer component with Link

--- src/core/footer/component.tsx
+++ src/core/footer/component.tsx
@@ -1,13 +1,22 @@
 import * as React from 'react';
+import { Link } from 'react-router';
 
 const Footer = () => (
   <div className="block block-bordered-lg" id="footer-block">
     <div className="container">
       <div className="row">
-        <a href="/#/about" className="col-xs-3 text-center margin-bottom-md"><h6 className="text-uppercase">About</h6></a>
-        <a href="/#/careers" className="col-xs-3 text-center margin-bottom-md"><h6 className="text-uppercase">Careers</h6></a>
-        <a href="/#/faq" className="col-xs-3 text-center margin-bottom-md"><h6 className="text-uppercase">FAQ</h6></a>
-        <a href="/#/contact" className="col-xs-3 text-center margin-bottom-md"><h6 className="text-uppercase">Contact</h6></a>
+        <Link to="about" className="col-xs-3 text-center margin-bottom-md">
+          <h6 className="text-uppercase">About</h6>
+        </Link>
+        <Link to="careers" className="col-xs-3 text-center margin-bottom-md">
+          <h6 className="text-uppercase">Careers</h6>
+        </Link>
+        <Link to="faq" className="col-xs-3 text-center margin-bottom-md">
+          <h6 className="text-uppercase">FAQ</h6>
+        </Link>
+        <Link to="contact" className="col-xs-3 text-center margin-bottom-md">
+          <h6 className="text-uppercase">Contact</h6>
+        </Link>
       </div>
     </div>
   </div>
