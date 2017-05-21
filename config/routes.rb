dc93d6d78f04218e24984d89bd86f8cfd0668092
David Hu <davidhu314@gmail.com>
2017-05-20 17:09:46 -0700

    update formatting for routes, fixed confirmation link

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
 
   namespace :api do
     defaults format: :json do
-      resources :users, only: [:create, :show, :update]
-      resources :universities, only: [:index, :show]
-      resources :posts, only: [:index, :create, :show, :update, :destroy]
-      resources :courses, only: [:index, :show]
-      resources :categories, only: [:index, :show]
-      resources :search, only: [:index]
-      resources :bookmarks, only: [:index, :create, :destroy]
-      resources :rfps, only: [:index, :create, :destroy]
+      resources :users,           only: [:create, :show, :update]
+      resources :universities,    only: [:index, :show]
+      resources :posts,           only: [:index, :create, :show, :update, :destroy]
+      resources :courses,         only: [:index, :show]
+      resources :categories,      only: [:index, :show]
+      resources :search,          only: [:index]
+      resources :bookmarks,       only: [:index, :create, :destroy]
+      resources :rfps,            only: [:index, :create, :destroy]
     end
   end
 
