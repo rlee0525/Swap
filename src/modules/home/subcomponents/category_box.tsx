f81b4856288963cd14c69545aaeead16ae6a7257
David Hu <davidhu314@gmail.com>
2017-05-23 21:35:24 -0700

    update formatting

--- src/modules/home/subcomponents/category_box.tsx
+++ src/modules/home/subcomponents/category_box.tsx
@@ -2,25 +2,25 @@ import React from 'react';
 import { Link } from 'react-router';
 
 interface ICategory {
-  link: string;
-  title: string;
-  description: string;
-  image: string;
+  link : string;
+  title : string;
+  description : string;
+  image : string;
 }
 
 interface Props {
-  viewDescription: (e: any) => void;
-  hideDescription: (e: any) => void;
-  category: ICategory;
+  viewDescription : (e: any) => void;
+  hideDescription : (e: any) => void;
+  category : ICategory;
 }
 
 const CategoryBox: React.SFC<Props> = ({ viewDescription, hideDescription, category }) => (
   <div className="col-xs-6 col-md-4">
     <Link to={category.link} className="thumbnail" onMouseOver={viewDescription} onMouseOut={hideDescription}>
-      <img className="thumbnail-image" src={ category.image } alt="..."/>
+      <img className="thumbnail-image" src={category.image} alt="..."/>
       <div className="carousel-caption">
-        <h3 className="category-title">{ category.title }</h3>
-        <p className="category-description hide">{ category.description }</p>
+        <h3 className="category-title">{category.title}</h3>
+        <p className="category-description hide">{category.description}</p>
       </div>
     </Link>
   </div>
