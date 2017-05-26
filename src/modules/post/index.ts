1915e65bb799bd69ca244f97def0410db3c95c2a
randyjap <randyjap@gmail.com>
2017-05-25 21:17:16 -0700

    update view count without updated_at

--- src/modules/post/index.ts
+++ src/modules/post/index.ts
@@ -1,23 +1,32 @@
 import { connect } from 'react-redux';
 import Post from './component';
 import { getPost } from './actions';
+import { search, saveQuery } from './actions';
 
 interface StateProps {
   user: any;
   post: object;
+  searchResult: any;
+  currentQuery: any;
 }
 
 interface DispatchProps {
   getPost: (id: number, access_token: string) => void;
+  search: (query: object) => void;
+  saveQuery: (query: string) => void;
 }
 
 const mapStateToProps = (state: any, ownProp?: any): StateProps => ({
   user: state.user,
-  post: state.post
+  post: state.post,
+  searchResult: state.searchResult,
+  currentQuery: state.currentQuery
 });
 
 const mapDispatchToProps = (dispatch: any): DispatchProps => ({
-  getPost: (id, access_token) => dispatch(getPost(id, access_token))
+  getPost: (id, access_token) => dispatch(getPost(id, access_token)),
+  search: query => dispatch(search(query)),
+  saveQuery: query => dispatch(saveQuery(query))
 });
 
 export default connect(
