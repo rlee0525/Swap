42a14e8981e6f9fc79450c21db278bb8b15c18ce
randyjap <randyjap@gmail.com>
2017-05-26 15:06:51 -0700

    add my_course controller and jbuilder view

--- app/controllers/api/my_courses_controller.rb
+++ app/controllers/api/my_courses_controller.rb
@@ -1,2 +1,15 @@
 class Api::MyCoursesController < ApplicationController
+  def index
+    user = fb_auth_user(params[:access_token])
+    @my_courses = user.courses
+    render "api/my_courses/index", status: 200
+  end
+
+  def destroy
+    user = fb_auth_user(params[:access_token])
+    @my_courses = UsersCourse.where(user: user)
+                  .where(course: Course.find(params[:id]))
+    @my_courses.each(&:destroy)
+    render json: ["successfully deleted"], status: 200
+  end
 end
