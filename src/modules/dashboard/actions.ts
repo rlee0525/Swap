111eb3c8d97a2c59a1acd349fc854954239d03d7
David Hu <davidhu314@gmail.com>
2017-05-25 19:04:00 -0700

    implemented my post actions

--- src/modules/dashboard/actions.ts
+++ src/modules/dashboard/actions.ts
@@ -2,15 +2,36 @@ import * as DashboardAPI from './utils';
 
 export const DASHBOARD = {
   MY_POSTS: 'MY_POSTS',
+  DELETE_POST: 'DELETE_POST',
   BOOKMARKS: 'BOOKMARKS',
   DELETE_BOOKMARK: 'DELETE_BOOKMARK',
   RFPS: 'RFPS'
 }
 
+// my posts actions
 export const receiveMyPosts = posts => ({
   type: DASHBOARD.MY_POSTS,
   posts
 });
+
+export const removeMyPost = id => ({
+  type: DASHBOARD.DELETE_POST,
+  id
+});
+
+export const fetchMyPosts = accessToken => dispatch => (
+  DashboardAPI.fetchMyPosts(accessToken).then(
+    res => dispatch(receiveMyPosts(res)),
+    err => console.log(err)
+  )
+);
+
+export const deleteMyPost = (id, accessToken) => dispatch => (
+  DashboardAPI.deleteMyPost(id, accessToken).then(
+    () => dispatch(removeMyPost(id)),
+    err => console.log(err)
+  )
+);
 
 // bookmark actions
 export const receiveBookmarks = bookmarks => ({
