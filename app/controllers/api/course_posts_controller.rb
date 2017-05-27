1dd6666c927102877408ddb8050f5c09b86d4333
randyjap <randyjap@gmail.com>
2017-05-26 18:51:16 -0700

    fix controller view template

--- app/controllers/api/course_posts_controller.rb
+++ app/controllers/api/course_posts_controller.rb
@@ -3,7 +3,7 @@ class Api::CoursePostsController < ApplicationController
     user = fb_auth_user(params[:access_token])
     if user
       @course_posts = user.course_posts
-      render "api/my_courses/show", status: 200
+      render "api/course_posts/index", status: 200
     else
       render json: ["unauthorized"], status: 403
     end
