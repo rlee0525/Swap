1634a5fa41e8be53d63abda7d5d5c33c81a28c7f
randyjap <randyjap@gmail.com>
2017-05-26 18:45:05 -0700

    add course_posts index method

--- app/controllers/api/course_posts_controller.rb
+++ app/controllers/api/course_posts_controller.rb
@@ -1,2 +1,11 @@
 class Api::CoursePostsController < ApplicationController
+  def index
+    user = fb_auth_user(params[:access_token])
+    if user
+      @course_posts = user.course_posts
+      render "api/my_courses/show", status: 200
+    else
+      render json: ["unauthorized"], status: 403
+    end
+  end
 end
