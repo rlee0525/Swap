0beffacbff31c626a1e808e2195f579ff591f74f
David Hu <davidhu314@gmail.com>
2017-05-29 19:57:10 -0700

    add firebase to dependencies

--- config/api_key.js
+++ config/api_key.js
@@ -1,3 +1,11 @@
 module.exports = {
-  appId: '1843174039341965'
+  appId: '1843174039341965',
+  firebase: {
+    apiKey: "AIzaSyBmZ6O-6cQAiKLWRhk90Gn6NgcG9sVpR5E",
+    authDomain: "swapnow-a0d27.firebaseapp.com",
+    databaseURL: "https://swapnow-a0d27.firebaseio.com",
+    projectId: "swapnow-a0d27",
+    storageBucket: "swapnow-a0d27.appspot.com",
+    messagingSenderId: "961625148410"
+  }
 };
