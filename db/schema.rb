a15a89bebd8334792ef225a103f551d4349f58c3
David Hu <davidhu314@gmail.com>
2017-05-29 19:40:53 -0700

    create conversations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170528214040) do
+ActiveRecord::Schema.define(version: 20170530024021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20170528214040) do
     t.integer  "post_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "conversation_id",                 null: false
+    t.integer  "user_id",                         null: false
+    t.boolean  "archived",        default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
   create_table "courses", force: :cascade do |t|
---
+++ spec/factories/conversations.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :conversation do
+    conversation_id 1
+    user_id 1
+    archived false
+  end
+end
