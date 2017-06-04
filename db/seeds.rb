c819850fe271a2877fdacf8734e8517cf2dd0da0
Raymond Lee <raylee0525@gmail.com>
2017-06-04 16:15:59 -0700

    Add David to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,6 +41,15 @@ User.create!(
   last_name: ENV['RANDY_LAST_NAME']
 )
 
+User.create!(
+  fb_id: ENV['DAVID_FB_ID'],
+  edu_email: "bobbybobby@berkeley.edu",
+  university: University.first,
+  fb_picture: ENV['DAVID_FB_PICTURE'],
+  first_name: ENV['DAVID_FIRST_NAME'],
+  last_name: ENV['DAVID_LAST_NAME']
+)
+
 200.times do |idx|
   category = categories.sample
   course = nil
