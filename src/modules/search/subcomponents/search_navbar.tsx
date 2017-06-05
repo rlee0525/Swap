4312199b93ed9809547b4d4c652b2e7600b5a622
Raymond Lee <raylee0525@gmail.com>
2017-06-05 12:01:54 -0700

    make search navbar completely responsive

--- src/modules/search/subcomponents/search_navbar.tsx
+++ src/modules/search/subcomponents/search_navbar.tsx
@@ -97,26 +97,28 @@ class SearchNavbar extends React.Component<Props, State> {
     return (
       <div className="container" id="search-navbar-container">
         <div className="row">
-          <div className="dropdown col-md-2" id="no-padding-left">
-            <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-              {this.state.category || this.props.currentQuery.category}
-              <span className="caret" id="special-caret"></span>
-            </button>
-            <ul className="dropdown-menu col-md-2" aria-labelledby="dropdownMenu1">
-              <li><a onClick={() => this.renderCategoryMenu("All")}>All</a></li>
-              <li><a onClick={() => this.renderCategoryMenu("Course Material")}>Course Material</a></li>
-              <li><a onClick={() => this.renderCategoryMenu("Furniture")}>Furniture</a></li>
-              <li><a onClick={() => this.renderCategoryMenu("Clothing")}>Clothing</a></li>
-              <li><a onClick={() => this.renderCategoryMenu("Electronics")}>Electronics</a></li>
-              <li><a onClick={() => this.renderCategoryMenu("Housing")}>Housing</a></li>
-              <li><a onClick={() => this.renderCategoryMenu("Bikes")}>Bikes</a></li>
-              <li><a onClick={() => this.renderCategoryMenu("Games")}>Games</a></li>
-              <li><a onClick={() => this.renderCategoryMenu("Others")}>Others</a></li>
-              <li><a onClick={() => this.renderCategoryMenu("Lost & Found")}>Lost & Found</a></li>
-              <li className={this.props.user ? "" : "hidden"}><a onClick={() => this.renderCategoryMenu("My Course Material")}>My Course Material</a></li>
-            </ul>
+          <div className="col-md-2 col-sm-2 row" id="no-padding-left">
+            <div className="dropdown search-dropdown">
+              <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                {this.state.category || this.props.currentQuery.category}
+                <span className="caret" id="special-caret"></span>
+              </button>
+              <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
+                <li><a onClick={() => this.renderCategoryMenu("All")}>All</a></li>
+                <li><a onClick={() => this.renderCategoryMenu("Course Material")}>Course Material</a></li>
+                <li><a onClick={() => this.renderCategoryMenu("Furniture")}>Furniture</a></li>
+                <li><a onClick={() => this.renderCategoryMenu("Clothing")}>Clothing</a></li>
+                <li><a onClick={() => this.renderCategoryMenu("Electronics")}>Electronics</a></li>
+                <li><a onClick={() => this.renderCategoryMenu("Housing")}>Housing</a></li>
+                <li><a onClick={() => this.renderCategoryMenu("Bikes")}>Bikes</a></li>
+                <li><a onClick={() => this.renderCategoryMenu("Games")}>Games</a></li>
+                <li><a onClick={() => this.renderCategoryMenu("Others")}>Others</a></li>
+                <li><a onClick={() => this.renderCategoryMenu("Lost & Found")}>Lost & Found</a></li>
+                <li className={this.props.user ? "" : "hidden"}><a onClick={() => this.renderCategoryMenu("My Course Material")}>My Course Material</a></li>
+              </ul>
+            </div>
           </div>
-          <div className="input-group col-md-10 col-sm-12 col-xs-8" id="phone-search-nav">
+          <div className="input-group col-md-10 col-sm-10" id="phone-search-nav">
             <input id="search-query" type="text" className="form-control" placeholder="Search" onChange={e => this.onChange(e)}
                   onKeyDown={e => this.checkKey(e)} value={this.props.currentQuery.query} />
             <span className="input-group-addon search-icon" id="basic-addon2" onClick={this.enterSearchQuery}><span className="glyphicon glyphicon-search" aria-hidden="true" /></span>
