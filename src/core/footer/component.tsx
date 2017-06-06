e7a3b1c27b8eeffae0351cff6a7756a73116c719
Raymond Lee <raylee0525@gmail.com>
2017-06-05 19:35:45 -0700

    create a sticky footer

--- src/core/footer/component.tsx
+++ src/core/footer/component.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Link } from 'react-router';
 
 const Footer = () => (
-  <div className="block block-bordered-lg" id="footer-block">
+  <div className="block block-bordered-lg absolute-footer" id="footer-block">
     <div className="container">
       <div className="row">
         <Link to="infos?about" className="col-xs-3 text-center margin-bottom-md no-hover">
