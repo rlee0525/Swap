5af0dc2d874ea40424dc2468b83a52914c14e209
David Hu <davidhu314@gmail.com>
2017-06-08 16:14:40 -0700

    add styling to chat to ensure the input stay at the bottom

--- src/modules/chat/subcomponents/messages.tsx
+++ src/modules/chat/subcomponents/messages.tsx
@@ -12,9 +12,6 @@ class Messages extends React.Component<Props, {}> {
   render() {
     let { conversation, user } = this.props;
 
-    console.log(conversation);
-    
-
     let timestamps = Object.keys(conversation.messages).reverse();
     let renderedDelivered = false;
     let renderedSeen = false;
