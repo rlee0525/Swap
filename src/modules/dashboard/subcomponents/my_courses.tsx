a52a53a353c7f5eb263852acc23d83e586cbc967
Raymond Lee <raylee0525@gmail.com>
2017-06-26 13:36:24 -0700

    update new course modal

--- src/modules/dashboard/subcomponents/my_courses.tsx
+++ src/modules/dashboard/subcomponents/my_courses.tsx
@@ -139,7 +139,7 @@ class MyCourses extends React.Component<Props, State> {
             <div className="modal-content">
               <div className="modal-header" id="auth-modal-header">
                 <button type="button" className="close" data-dismiss="modal">&times;</button>
-                <h3 className="modal-title" id="authModalLabel">Add Course</h3>
+                <h3 className="modal-title" id="authModalLabel">Add a new course</h3>
               </div>
               <div className="modal-body text-center" id="fb-modal-body">
                 <form onSubmit={ this.submitForm }>
