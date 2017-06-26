9ace98491f3d77228d4156969d75590622a08d35
Raymond Lee <raylee0525@gmail.com>
2017-06-26 15:34:25 -0700

    responsive dashboard for phone view

--- src/modules/dashboard/subcomponents/dashboard_headers.tsx
+++ src/modules/dashboard/subcomponents/dashboard_headers.tsx
@@ -12,25 +12,120 @@ interface Props {
   button: null | { title: string; action: any }
 }
 
-const DashboardHeaders : React.SFC<Props> = ({ active, headers, button }) => (
-  <ul className="nav nav-tabs">
-    { headers.map(header => (
-      <li role="presentation" id="dashboard-nav-title" className={header.link === active ? 'active' : ''}>
-        <Link to={`dashboard?${header.link}`}>{ header.title }</Link>
-      </li>
-    ))}
+interface State {
+  headerTitle: string;
+}
 
-    { button ?  (
-      <div>
-        <button onClick={button.action} className="btn btn-clear nav-button" id="responsive-create-text">
-          { button.title }
-        </button>
-        <button onClick={button.action} className="btn btn-clear nav-button" id="responsive-create-icon">
-          <span className="glyphicon glyphicon-edit" aria-hidden="true" id="create-icon-button"/>
-        </button>
-      </div>
-    ) : null }
-  </ul>
-);
+class DashboardHeaders extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      headerTitle: "Posts"
+    };
+
+    this.changeTab = this.changeTab.bind(this);
+  }
+
+  changeTab(headerTitle) {
+    this.setState({ headerTitle });
+  }
+
+  renderHeaders() {
+    let { active, headers } = this.props;
+    let windowSize = window.innerWidth > 450;
+
+    return (
+      headers.map(header => (
+        <li 
+          role="presentation" 
+          id="dashboard-nav-title" 
+          className={header.link === active ? 'active' : ''}
+        >
+          <Link 
+            to={`dashboard?${header.link}`}
+            onClick={() => this.changeTab(header.title)}
+          >
+            { header.title }
+          </Link>
+        </li>
+      ))
+    );
+  }
+
+  renderButtons() {
+    let button = this.props.button;
+    let windowSize = window.innerWidth > 450;
+
+    if (button) {
+      return (
+        <div className={windowSize ? "" : "col-xs-2"}>
+          <button 
+            onClick={button.action} 
+            className="btn btn-clear nav-button" 
+            id="responsive-create-text"
+          >
+            { button.title }
+          </button>
+          <button 
+            onClick={button.action} 
+            className="btn btn-clear nav-button" 
+            id="responsive-create-icon"
+          >
+            <span 
+              className="glyphicon glyphicon-edit" 
+              aria-hidden="true" 
+              id="create-icon-button"
+            />
+          </button>
+        </div>
+      )
+    }
+
+    return null;
+  }
+
+  render() {
+    let { active, headers, button } = this.props;
+    let windowSize = window.innerWidth > 450;
+
+    if (windowSize) {
+      return (
+        <ul className="nav nav-tabs">
+          {this.renderHeaders()}
+          {this.renderButtons()}
+        </ul>
+      );
+    } else {
+      return (
+        <div className="row">
+          <div className="col-xs-9">
+            <div className="dropdown dashboard-dropdown">
+              <button 
+                type="button" 
+                id="dropdownMenu2" 
+                aria-haspopup="true" 
+                aria-expanded="true"
+                data-toggle="dropdown" 
+                className="btn btn-default dropdown-toggle" 
+              >
+                {this.state.headerTitle}
+                <span className="caret" id="special-caret" />
+              </button>
+              <ul 
+                className="dropdown-menu category-dropdown" 
+                aria-labelledby="dropdownMenu2"
+              >
+                {this.renderHeaders()}
+              </ul>
+            </div>
+          </div>
+
+          {this.renderButtons()}
+        </div>
+      );
+    }
+  }
+}
 
 export { DashboardHeaders };
