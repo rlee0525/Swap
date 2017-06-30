89edb5f51a6edd4620c416a4ccf48796ff08060c
Raymond Lee <raylee0525@gmail.com>
2017-06-30 16:53:24 -0700

    responsive chat - web view

--- src/modules/chat/subcomponents/conversation_item.tsx
+++ src/modules/chat/subcomponents/conversation_item.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { shortenString } from 'helpers';
+
 interface Props {
   user: any;
   active: boolean;
@@ -7,12 +9,24 @@ interface Props {
   changeConversation: () => void;
 }
 
-const ConversationItem : React.SFC<Props> = ({ user, changeConversation, hasUnreadMessages, active }) => (
-  <div className={`conversation-item ${active ? 'active-chat' : ''}`} onClick={changeConversation}>
-    { hasUnreadMessages ? <div className='noti unread' /> : <div className='noti' /> }
-    <img src={user.fb_picture} alt='Profile Picture' />
-    <span>{`${user.first_name} ${user.last_name}`}</span>
-  </div>
-);
+class ConversationItem extends React.Component<Props, any> {
+  render() {
+    let { user, changeConversation, hasUnreadMessages, active } = this.props;
+    let name = `${user.first_name} ${user.last_name}`;
+    
+    return (
+      <div className={`conversation-item ${active ? 'active-chat' : ''}`} onClick={changeConversation}>
+        { hasUnreadMessages ? <div className='noti unread' /> : <div className='noti' /> }
+        <img src={user.fb_picture} alt='Profile Picture' />
+        <span className="chat-name-1">
+          {shortenString(name, 15)}
+        </span>
+        <span className="chat-name-2">
+          {shortenString(user.first_name, 7)}
+        </span>
+      </div>
+    )
+  }
+}
 
 export { ConversationItem };
